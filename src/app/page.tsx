d4054f7653a1c452e625cb644afbe312eaff6e31
panduputragit <put.rariadi1144@gmail.com>
2024-05-02 14:14:32 +0800

    set-up-project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="font-extrabold">
+    <main className="font-extrabold bg-light w-full min-h-screen">
       test
     </main>
   );
